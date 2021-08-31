c0f93289b3d384da9443d56aed478de1d314fbeb
piaoliangkb <piaoliangkb@gmail.com>
2021-08-31 22:57:30 +0800

    Add shared_ptr in native object

--- jni-cpp-class-wrapper/JNIDemo.cpp
+++ jni-cpp-class-wrapper/JNIDemo.cpp
@@ -3,20 +3,32 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <vector>
 
 class FooBar {
 public:
     FooBar(int foo, std::string bar) {
         this->foo = foo;
         this->bar = bar;
+        std::vector<int> v {1, 2, 3, 4, 5};
+        this->pv = std::make_shared<std::vector<int>>(v);
     }
 
     std::string getStr() {
         return bar;
     }
+
+    void printVec() {
+        for (int i: *pv) {
+            std::cout << i << " ";
+        }
+        std::cout << std::endl;
+    }
+
 private:
     int foo;
     std::string bar;
+    std::shared_ptr<std::vector<int>> pv;
 };
 
 JNIEXPORT jlong JNICALL Java_JNIDemo_createNativeInstance(JNIEnv *, jobject thisObj) {
@@ -32,8 +44,8 @@ JNIEXPORT void JNICALL Java_JNIDemo_destroyNativeInstance(JNIEnv *, jobject this
 JNIEXPORT jstring JNICALL Java_JNIDemo_nativeMethod(JNIEnv* jenv, jobject thisObj, jlong native_ptr) {
     // Print int and string in FooBar instance
     FooBar* foobar = (FooBar*)native_ptr;
-    std::cout << foobar->getStr() << std::endl;
     jstring javaRet = jenv->NewStringUTF(foobar->getStr().c_str());
+    foobar->printVec();
     return javaRet;
 }
 
